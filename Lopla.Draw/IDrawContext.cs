fdc70b4a7f60cbad86e13a5f21906f7cdc829818
Paweł Gałęzowski <pgalezowski@gmail.com>
2018-12-23 23:37:37 +0100

    fixes

---
+++ Lopla.Draw/IDrawContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Lopla.Draw
+{
+    using System.IO;
+    using Lopla.Draw.Messages;
+
+    public interface IDrawContext
+    {
+        Stream GetResourceStream(string folder, string name);
+
+        Point CanvasSize();
+    }
+}
