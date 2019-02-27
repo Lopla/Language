8f4276226823244e4f258535dafed64f55bff106
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2019-02-27 16:53:24 +0100

    fix

---
+++ Lopla.Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Lopla.Language.Interfaces;
+using Lopla.Language.Processing;
+using Lopla.Language.Providers;
+using Lopla.Libs;
+
+namespace Lopla.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RunProject();
+        }
+
+        private static void RunProject()
+        {
+            //// project is poor 
+            //// only few dll's
+            var p = new Runner();
+            var runtime = p.Run(
+                new MemoryScripts("Test",
+                    new List<ILibrary>
+                    {
+                        new IO(),
+                        new Lp()
+                    },
+                    @"
+libs = Lp.Functions()
+k =0
+
+while(k<Lp.Len(libs)) {
+    d = libs[k]
+    IO.WriteLine(d[0])
+    k=k+1
+}
+IO.WriteLine(""test"")
+"));
+            if (runtime.HasErrors)
+                System.Console.WriteLine(runtime.ToString());
+        }
+    }
+}
