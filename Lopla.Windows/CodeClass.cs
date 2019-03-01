d6999f734be4eddfac0b2722805c818f9fa2586f
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2019-03-01 22:25:17 +0100

    fixed test

--- Lopla.Windows/CodeClass.cs
+++ Lopla.Windows/CodeClass.cs
@@ -16,10 +16,23 @@ function Perf.Stop(){
     ticksElapsed = Lp.Ticks()
 
     duration =ticksElapsed - lastCall 
-    seconds =(duration) / 10000000 
-    Draw.Log(seconds)
+    seconds = (duration) /   10000000 
+    Perf.Show()
 }
 
+function Perf.Show(){
+    Draw.SetColor(255,255,255)
+
+    ticksElapsed = Lp.Ticks()
+    duration = ticksElapsed - lastCall 
+    seconds = (duration) /   10000000 
+    
+    
+    Draw.SetColor(0,0,0)
+    Draw.Box(0,0, 100, 30)
+    Draw.SetColor(255,255,255)
+    Draw.Text(10,10, seconds)
+}
 ";
 
 
@@ -32,19 +45,26 @@ function Draw.Test(){
     Perf.Start()
 
     Draw.Clear(0,0,0)
-    Draw.SetColor(0,255,0)
     line = 0
     while(line < 1000){
-    col = 0
-    while(col < 100){
-        Draw.Line(line,col, line,col+1)
-        col = col +1
-    }
-    line = line + 1
-    Draw.Flush()
+        col = 0
+        while(col < 100){
+        
+            Draw.SetColor(0,255,0)
+            Draw.Line(line,col, line,col+1)
+            col = col +1
+            
+            Perf.Show()
+
+
+        }
+        Draw.Flush()
+        line = line + 1
+        
     }
 
     Draw.SetColor(255,255,255)
+
     Perf.Stop()
     Draw.Flush()
 }
