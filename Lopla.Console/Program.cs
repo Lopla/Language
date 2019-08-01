14a460cf79010eca586ca5c5745c0c6f67c43822
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2019-08-01 15:03:29 +0200

    fix

--- Lopla.Console/Program.cs
+++ Lopla.Console/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using Lopla.Language.Interfaces;
-using Lopla.Language.Processing;
-using Lopla.Language.Providers;
 using Lopla.Libs;
+using Lopla.Starting;
 
 namespace Lopla.Console
 {
@@ -11,57 +9,11 @@ namespace Lopla.Console
     {
         private static void Main(string[] args)
         {
-            if (args != null && args.Length > 0)
+            new MainHandler(new List<ILibrary>
             {
-                RunProject(args);                
-            }
-            else
-            {
-                /*
-                    https://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/redirection.mspx
-                    cat .\test.lp  | dotnet run --project Lopla.Console
-                 */
-                string stdin = null;
-                if (System.Console.IsInputRedirected)
-                {
-                    using (StreamReader reader = new StreamReader(System.Console.OpenStandardInput(), System.Console.InputEncoding))
-                    {
-                        stdin = reader.ReadToEnd();
-                        RunProject(stdin);
-                    }
-                }
-            }
-        }
-
-        private static void RunProject(string text)
-        {
-            //// project is poor 
-            //// only few dll's
-            var p = new Runner();
-            var runtime = p.Run(
-                
-                new MemoryScripts("Stdin",
-                    new List<ILibrary>
-                    {
-                        new IO(),
-                        new Lp()
-                    },text));
-            if (runtime.HasErrors)
-                System.Console.WriteLine(runtime.ToString());
-        }
-
-        private static void RunProject(string[] args)
-        {
-            var p = new Runner();
-            var runtime = p.Run(
-                new ProjectFromFolder(args[0],
-                    new List<ILibrary>
-                    {
-                        new IO(),
-                        new Lp()
-                    }));
-            if (runtime.HasErrors)
-                System.Console.WriteLine(runtime.ToString());
+                new IO(),
+                new Lp()
+            }).Main(args);
         }
     }
 }
---
+++ Lopla.Starting/MainHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Lopla.Language.Interfaces;
+using Lopla.Language.Processing;
+using Lopla.Language.Providers;
+
+namespace Lopla.Starting
+{
+    public class MainHandler
+    {
+        public MainHandler(List<ILibrary> list)
+        {
+            Libs = list;
+        }
+
+        private List<ILibrary> Libs { get; }
+
+        public void Main(string[] args)
+        {
+            var project = GetProject(args);
+
+            if(project == null)
+                return;
+            
+            var p = new Runner();
+            var runtime = p.Run(project);
+
+            if (runtime.HasErrors)
+                Console.WriteLine(runtime.ToString());
+        }
+
+        public IProject GetProject(string[] args)
+        {
+            if (args != null && args.Length > 0) return new ProjectFromFolder(args[0], Libs);
+
+            /*
+                    https://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/redirection.mspx
+                    cat .\test.lp  | dotnet run --project Lopla.Console
+                 */
+            string stdin = null;
+            if (Console.IsInputRedirected)
+                using (var reader = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding))
+                {
+                    stdin = reader.ReadToEnd();
+                    return new MemoryScripts("Stdin", Libs, stdin);
+                }
+
+            return null;
+        }
+    }
+}
