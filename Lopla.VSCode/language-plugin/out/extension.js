f65515f71705f93808930274501b0f53e9dac937
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2019-08-07 16:10:44 +0200

    added plugin

---
+++ Lopla.VSCode/language-plugin/out/extension.js
@@ -0,0 +1,102 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+const path = require("path");
+const execFile = require("child_process");
+let LoplaSchema = {};
+let LoplaKeywords = ['function', 'while', 'if', 'return'];
+function pupulateFunctionArgs() {
+    for (const key of Object.keys(LoplaSchema)) {
+        for (const methods of Object.keys(LoplaSchema[key])) {
+            console.log(key + "." + methods);
+        }
+    }
+}
+function getAvailbleFunctions() {
+    var loplaToolPath = path.resolve(__dirname, '../resources/loplac-win-x64');
+    var loplaTool = path.resolve(__filename, loplaToolPath, "loplac.exe");
+    var loplaScripsPath = path.resolve(__dirname, '../resources/scripts');
+    execFile.execFile(loplaTool, [loplaScripsPath], {}, (error, stdout, stderr) => {
+        var r = new RegExp("([a-zA-Z]+)[.]([a-zA-Z]+)");
+        if (stdout) {
+            console.log(stdout);
+            var lines = stdout.split("\n");
+            lines.forEach(function (line) {
+                let t = line.trim();
+                var e = r.exec(t);
+                if (e && e.length > 2) {
+                    var schema = e[1];
+                    var method = e[2];
+                    if (!LoplaSchema[schema])
+                        LoplaSchema[schema] = {};
+                    LoplaSchema[schema][method] = {};
+                }
+            });
+        }
+        pupulateFunctionArgs();
+    });
+}
+function activate(context) {
+    getAvailbleFunctions();
+    context.subscriptions.push(vscode.commands.registerCommand('extension.lopla', () => {
+        vscode.window.showInformationMessage('Lopla extension loaded');
+    }));
+    var loplaDocumentScheme = { scheme: 'file', language: 'lopla' };
+    context.subscriptions.push(vscode.languages.registerHoverProvider(loplaDocumentScheme, {
+        provideHover(document, position, token) {
+            return {
+                contents: ['Lopla file']
+            };
+        }
+    }));
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(loplaDocumentScheme, {
+        provideCompletionItems(document, position) {
+            let suggestions = [];
+            for (const key of Object.keys(LoplaSchema)) {
+                suggestions.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Module));
+            }
+            for (const val of LoplaKeywords) {
+                suggestions.push(new vscode.CompletionItem(val, vscode.CompletionItemKind.Keyword));
+            }
+            return suggestions;
+        }
+    }));
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(loplaDocumentScheme, {
+        provideCompletionItems(document, position) {
+            let p = position.translate(0, -position.character);
+            let line = document.getText(new vscode.Range(p, position));
+            for (const key of Object.keys(LoplaSchema)) {
+                let rs = '\\b' + key + '\\.';
+                let r = new RegExp(rs);
+                if (line && r.test(line)) {
+                    var suggestions = [];
+                    var methods = Object.keys(LoplaSchema[key]);
+                    methods.forEach(element => {
+                        suggestions.push(new vscode.CompletionItem(element, vscode.CompletionItemKind.Function));
+                    });
+                    return suggestions;
+                }
+            }
+            return null;
+        }
+    }, "."));
+    // context.subscriptions.push(
+    //   vscode.languages.registerCompletionItemProvider('lopla', {
+    //     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+    //       var fCompleteationitem = new vscode.CompletionItem('function', vscode.CompletionItemKind.Keyword);
+    //       fCompleteationitem.detail= 'Label for function';
+    //       fCompleteationitem.documentation = new vscode.MarkdownString("## DOCS");
+    //       return [
+    //         fCompleteationitem,
+    //         new vscode.CompletionItem('if', vscode.CompletionItemKind.Keyword),
+    //         new vscode.CompletionItem('while', vscode.CompletionItemKind.Keyword),
+    //         new vscode.CompletionItem('return', vscode.CompletionItemKind.Module)];
+    //     }
+    //   })
+    // );
+}
+exports.activate = activate;
+function deactivate() {
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
