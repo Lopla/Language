c4cb3b657beff7b25bef19039b442e30d29ba183
Paweł Gałęzowski <pgalezowski@gmail.com>
2020-03-24 20:12:54 +0100

    end line not needed?

--- Lopla.Language/compile.bat
+++ Lopla.Language/compile.bat
@@ -1,2 +1,3 @@
 @rem..\Bin\hime-3.3.1\himecc.bat Lopla.gram -n Lopla.Grammar -o:debug
-@..\Bin\hime-3.3.2\himecc.bat ..\Lopla.Language\Grammar\Lopla.gram -n Lopla.Language.Grammar  -p ..\Lopla.Language\Grammar
+@..\Bin\hime-3.4.0\himecc.bat ..\Lopla.Language\Grammar\Lopla.gram -n Lopla.Language.Grammar  -p ..\Lopla.Language\Grammar
+
