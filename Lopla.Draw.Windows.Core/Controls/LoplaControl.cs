92b21fb8088b3cddc702659c4d2eb18ec92b5d6b
Pawel Galezowski <pgalezowski@gmail.com>
2020-07-09 11:37:42 +0200

    control fix

--- Lopla.Draw.Windows.Core/Controls/LoplaControl.cs
+++ Lopla.Draw.Windows.Core/Controls/LoplaControl.cs
@@ -39,5 +39,80 @@ namespace Lopla.Draw.Windows.Controls
         {
             var drawCtx = new LoplaRequests(skControl1);
 
-            Engine =  new SkiaD
-            
+            Engine =  new SkiaDrawLoplaEngine(drawCtx);
+            _uiEventsProvider = new LoplaGuiEventProcessor(Engine);
+
+            var windowsDesktopEvents =
+                new WindowsDesktopEvents(skControl1, _uiEventsProvider);
+        }
+
+        public void Run()
+        {
+            _loplaThread = new Thread(StartWorker);
+            _loplaThread.Start();
+        }
+
+        private void StartWorker()
+        {
+            this._runner = new Runner();
+            if (_project != null)
+            {
+                var result = _runner.Run(_project);
+
+                if (this?.ParentForm?.Visible == true && this.ParentConsole == false)
+                {
+                    if (result.HasErrors) new LoplaErrors(result.ToString()).ShowDialog();
+                }
+                else
+                {
+                    Console.WriteLine(result.ToString());
+                }
+            }
+
+            OnLoplaDone?.Invoke(this, new EventArgs());
+        }
+
+        public void Project(string[] code)
+        {
+            _project = new MainHandler(new List<ILibrary>
+            {
+                new WinFormsDraw(ParentForm, Engine, _uiEventsProvider.UiEvents),
+                new Lp(code?.ToList().Skip(1).ToArray()),
+                new IO()
+            }).GetProject(code);
+        }
+
+        public void Project(IProject project)
+        {
+            this._project = project;
+        }
+
+        public ISender UiEvents => this._uiEventsProvider.UiEvents;
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+        }
+
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+        }
+
+        private void LoplaControl_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void Stop()
+        {
+            try{
+                _runner.Stop();
+                _loplaThread.Abort();
+                _uiEventsProvider.Stop();
+            }catch
+            {
+
+            }
+            OnLoplaDone?.Invoke(this, new EventArgs());
+        }
+    }
+}
